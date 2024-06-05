4faf2218d5f72203868469da2c6f8e24104bd4f4
Alexsandro de Lima Silva <alexsandrolima.dev@gmail.com>
2024-06-05 13:39:06 -0300

    12.20. Otimizando a query de pedidos e retornando model resumido na listagem

--- src/main/java/com/algaworks/algafood/api/controller/PedidoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/PedidoController.java
@@ -1,18 +1,19 @@
 package com.algaworks.algafood.api.controller;
 
-import java.util.List;
-
+import com.algaworks.algafood.api.assembler.PedidoModelAssembler;
+import com.algaworks.algafood.api.assembler.PedidoResumoModelAssembler;
+import com.algaworks.algafood.api.model.PedidoModel;
+import com.algaworks.algafood.api.model.PedidoResumoModel;
+import com.algaworks.algafood.domain.model.Pedido;
+import com.algaworks.algafood.domain.repository.PedidoRepository;
+import com.algaworks.algafood.domain.service.EmissaoPedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.algaworks.algafood.api.assembler.PedidoModelAssembler;
-import com.algaworks.algafood.api.model.PedidoModel;
-import com.algaworks.algafood.domain.model.Pedido;
-import com.algaworks.algafood.domain.repository.PedidoRepository;
-import com.algaworks.algafood.domain.service.EmissaoPedidoService;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/pedidos")
@@ -27,11 +28,14 @@ public class PedidoController {
     @Autowired
     private PedidoModelAssembler pedidoModelAssembler;
 
+    @Autowired
+    private PedidoResumoModelAssembler pedidoResumoModel;
+
     @GetMapping
-    public List<PedidoModel> listar() {
+    public List<PedidoResumoModel> listar() {
         List<Pedido> todosPedidos = pedidoRepository.findAll();
 
-        return pedidoModelAssembler.toCollectionModel(todosPedidos);
+        return pedidoResumoModel.toCollectionModel(todosPedidos);
     }
 
     @GetMapping("/{pedidoId}")
